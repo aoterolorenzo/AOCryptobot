97c12714f674201329aa002813e117b41cd53bca
aoterocom <aoterolorenzo@gmail.com>
2021-01-30 21:30:21 +0100

    hotfix: main.go quick refactor

--- main.go
+++ main.go
@@ -4,29 +4,17 @@ import (
 	"./marketmaker"
 	"./marketmaker/service/binance"
 	"os"
-	"sync"
 )
 
-var wg sync.WaitGroup
-
-
-
 func main() {
-
-
-
 	marketService := service.MarketService{}
 	marketService.SetPair(os.Getenv("pair"))
 	marketService.ConfigureClient()
 	marketService.StartMonitor()
-
 	run(&marketService)
-
-
 }
 
-
-func run(marketService *service.MarketService)  {
+func run(marketService *service.MarketService) {
 	strategy := marketmaker.MMStrategy{}
 	strategy.SetMarketService(marketService)
 	strategy.Execute()
