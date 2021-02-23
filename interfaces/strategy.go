e552536775e85f94854859581a6ed7bdda93a129
aoterocom <aoterolorenzo@gmail.com>
2021-02-23 01:07:14 +0100

    fix: !29 update custom strategy

--- interfaces/strategy.go
+++ interfaces/strategy.go
@@ -10,5 +10,5 @@ type Strategy interface {
 	ShouldExit(timeSeries *techan.TimeSeries) bool
 	ParametrizedShouldExit(timeSeries *techan.TimeSeries, constant float64) bool
 	ParametrizedShouldEnter(timeSeries *techan.TimeSeries, constant float64, trendPct float64) bool
-	PerformAnalysis(exchangeService ExchangeService, pair string, constants []float64) (analytics.PairAnalysis, error)
+	PerformAnalysis(exchangeService ExchangeService, pair string, interval string, constants []float64) (analytics.PairAnalysis, error)
 }
