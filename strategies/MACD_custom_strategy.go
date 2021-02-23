d71ff2dd44906ad2bb919068511521729fc0252f
aoterocom <aoterolorenzo@gmail.com>
2021-02-23 14:54:02 +0100

    fix: !29 update custom strategy with draft prints

--- strategies/MACD_custom_strategy.go
+++ strategies/MACD_custom_strategy.go
@@ -1,7 +1,6 @@
 package strategies
 
 import (
-	"fmt"
 	"github.com/sdcoffey/techan"
 	"gitlab.com/aoterocom/AOCryptobot/helpers"
 	"gitlab.com/aoterocom/AOCryptobot/interfaces"
@@ -49,20 +48,23 @@ func (s *MACDCustomStrategy) ParametrizedShouldEnter(timeSeries *techan.TimeSeri
 func (s *MACDCustomStrategy) ParametrizedShouldExit(timeSeries *techan.TimeSeries, constant float64) bool {
 
 	closePrices := techan.NewClosePriceIndicator(timeSeries)
+	lastCandleIndex := len(timeSeries.Candles) - 1
+
+	// Check y last candle is about to end
+	if time.Now().Unix()-120 < timeSeries.Candles[lastCandleIndex].Period.Start.Unix() {
+		return false
+	}
 
 	MACD := techan.NewMACDIndicator(closePrices, 12, 26)
 	MACDHistogram := techan.NewMACDHistogramIndicator(MACD, 9)
 
-	lastMACDHistogramValue := techan.NewConstantIndicator(MACDHistogram.Calculate(len(timeSeries.Candles) - 2).Float())
-	constant0dot15 := techan.NewConstantIndicator(constant)
+	currentMACDHistogramValue := MACDHistogram.Calculate(lastCandleIndex).Float()
+	lastMACDHistogramValue := MACDHistogram.Calculate(lastCandleIndex - 1).Float()
 
-	exitRule := techan.And(
-		techan.NewCrossDownIndicatorRule(lastMACDHistogramValue, MACDHistogram),
-		techan.NewCrossDownIndicatorRule(constant0dot15, MACDHistogram),
-	)
+	exitRuleSetCheck := currentMACDHistogramValue < lastMACDHistogramValue ||
+		currentMACDHistogramValue < constant
 
-	record := &techan.TradingRecord{}
-	return exitRule.IsSatisfied(len(timeSeries.Candles)-1, record)
+	return exitRuleSetCheck
 }
 
 func (s *MACDCustomStrategy) PerformAnalysis(exchangeService interfaces.ExchangeService, pair string, interval string, constants []float64) (analytics.PairAnalysis, error) {
@@ -79,20 +81,20 @@ func (s *MACDCustomStrategy) PerformAnalysis(exchangeService interfaces.Exchange
 	highestBalance := -1.0
 	highestEnterConstant := -1.0
 	highestExitConstant := -1.0
-	var selectedEntryConstant float64
-	var selectedExitConstant float64
 
 	balance := 1000.0
 	var buyRate float64
 	var sellRate float64
 	open := false
+	enterConstant := lastVal * -0.0002
+	exitConstant := lastVal * 0.001
+	enterStop := lastVal * 0.001
+	exitStop := lastVal * -0.0002
+
+	selectedEntryConstant := enterConstant
+	selectedExitConstant := exitConstant
 
 	for {
-		enterConstant := lastVal * -0.0002
-		exitConstant := lastVal * 0.001
-		enterStop := lastVal * 0.001
-		exitStop := lastVal * -0.0002
-
 		for ; enterConstant < enterStop; enterConstant += jump {
 			balance = 1000.0
 			for i := 4; i < len(series.Candles); i++ {
@@ -100,22 +102,30 @@ func (s *MACDCustomStrategy) PerformAnalysis(exchangeService interfaces.Exchange
 				newSeries := series
 				newSeries.Candles = candles
 
+				//fmt.Printf("%v\n", candles[i-1].Period.End.String())
 				if !open && len(candles) > 4 && s.ParametrizedShouldEnter(&newSeries, enterConstant, trendPctCondition) {
 					open = true
 					buyRate = candles[i-1].ClosePrice.Float()
+					//fmt.Printf("Buy at %f\n", buyRate)
 				} else if open && s.ParametrizedShouldExit(&newSeries, selectedExitConstant) {
 					open = false
 					sellRate = candles[i-1].ClosePrice.Float()
 					profitPct := sellRate * 1 / buyRate
 					balance *= profitPct * (1 - 0.00014)
+					/*fmt.Printf("Sell at %f\n", sellRate)
+					fmt.Printf("Profit %f\n", profitPct)
+					fmt.Printf("Balance %f\n", balance)*/
 				}
+
 			}
+			open = false
+			//return pairAnalysis, nil
 
 			if balance > highestBalance {
 				highestBalance = balance
 				selectedEntryConstant = enterConstant
 			}
-			fmt.Printf("Entry Constant: %.8f Exit Constant %.8f Balance: %.8f\n", enterConstant, exitConstant, balance)
+			//fmt.Printf("Entry Constant: %.8f Exit Constant %.8f Balance: %.8f\n", enterConstant, selectedExitConstant, balance)
 		}
 
 		for ; exitConstant > exitStop; exitConstant -= jump {
@@ -136,19 +146,20 @@ func (s *MACDCustomStrategy) PerformAnalysis(exchangeService interfaces.Exchange
 					balance *= profitPct * (1 - 0.00014)
 				}
 			}
+			open = false
 			if balance > highestBalance {
 				highestBalance = balance
 				selectedExitConstant = exitConstant
 			}
-			fmt.Printf("Entry Constant: %.8f Exit constant: %.8f Balance: %.8f\n", selectedEntryConstant, exitConstant, balance)
+			//fmt.Printf("Entry Constant: %.8f Exit constant: %.8f Balance: %.8f\n", selectedEntryConstant, exitConstant, balance)
 		}
 
 		if selectedEntryConstant != highestEnterConstant && selectedExitConstant != highestExitConstant {
 			highestEnterConstant = selectedEntryConstant
 			highestExitConstant = selectedExitConstant
 		} else {
-			fmt.Printf("%s: BEST CONSTANT COMBINATION FOUND: Entry Constant: %.8f Exit Constant %.8f Profit: %.4f%%\n",
-				pair, selectedEntryConstant, selectedExitConstant, highestBalance*100/1000-100)
+			//fmt.Printf("%s: BEST CONSTANT COMBINATION FOUND: Entry Constant: %.8f Exit Constant %f Profit: %.4f%%\n",
+			//	pair, selectedEntryConstant, selectedExitConstant, highestBalance*100/1000-100)
 			break
 		}
 	}
