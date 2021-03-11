455e64fef88a17080036691265a1ad935ca700cf
aoterocom <aoterolorenzo@gmail.com>
2021-03-11 16:27:00 +0100

    fix: !29 add implementation (draft)

--- bot_signaltrader/trader.go
+++ bot_signaltrader/trader.go
@@ -36,7 +36,7 @@ func (t *Trader) Start() {
 	t.firstExitTriggered = make(map[string]bool)
 	t.enterPrice = make(map[string]float64)
 	t.balance = 1016.859613
-	t.stopLoss = 0.00025
+	t.stopLoss = 0.0025
 	t.MaxOpenPositions = 3
 	t.OpenPositions = 0
 
