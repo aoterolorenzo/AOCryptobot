7d11ac932d40e241311a127cc7e8dc386a3d029f
aoterocom <aoterolorenzo@gmail.com>
2021-05-08 11:19:21 +0200

    Add strategy unit tests

---
+++ tests/strategies/Lun1Mar_custom_strategy_test.go
@@ -0,0 +1,32 @@
+package strategies
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"gitlab.com/aoterocom/AOCryptobot/helpers"
+	"gitlab.com/aoterocom/AOCryptobot/strategies"
+	"gitlab.com/aoterocom/AOCryptobot/tests/mocks"
+	"testing"
+)
+
+func TestLun1MarCustomStrategy(t *testing.T) {
+	strategy := strategies.NewLun1MarCustomStrategy()
+	exchangeService := &mocks.ProviderMock{}
+	symbol := "ETHEUR"
+	strategyResults, _ := strategy.PerformSimulation(symbol, exchangeService, "15m", 500, 0, nil)
+
+	ratio := helpers.PositiveNegativeRatio(strategyResults.ProfitList)
+	profit := strategyResults.Profit
+	constants := strategyResults.Constants
+
+	fmt.Printf("%s: Best combination last 500 candles: ", symbol)
+	for i, _ := range strategyResults.Constants {
+		fmt.Printf("Constant%d %.8f ", i+1, strategyResults.Constants[i])
+	}
+	fmt.Printf("Ratio %f Profit: %.2f%%\n", helpers.PositiveNegativeRatio(strategyResults.ProfitList),
+		strategyResults.Profit)
+
+	assert.Equal(t, 0.7857142857142857, ratio)
+	assert.Equal(t, 6.953072770110964, profit)
+	assert.Equal(t, []float64{0.005}, constants)
+}
---
+++ tests/strategies/stoch_rsi_custom_strategy_test.go
@@ -0,0 +1,32 @@
+package strategies
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"gitlab.com/aoterocom/AOCryptobot/helpers"
+	"gitlab.com/aoterocom/AOCryptobot/strategies"
+	"gitlab.com/aoterocom/AOCryptobot/tests/mocks"
+	"testing"
+)
+
+func TestStochRSICustomStrategy(t *testing.T) {
+	strategy := strategies.NewStochRSICustomStrategy()
+	exchangeService := &mocks.ProviderMock{}
+	symbol := "ETHEUR"
+	strategyResults, _ := strategy.PerformSimulation(symbol, exchangeService, "15m", 500, 0, nil)
+
+	ratio := helpers.PositiveNegativeRatio(strategyResults.ProfitList)
+	profit := strategyResults.Profit
+	constants := strategyResults.Constants
+
+	fmt.Printf("%s: Best combination last 500 candles: ", symbol)
+	for i, _ := range strategyResults.Constants {
+		fmt.Printf("Constant%d %.8f ", i+1, strategyResults.Constants[i])
+	}
+	fmt.Printf("Ratio %f Profit: %.2f%%\n", helpers.PositiveNegativeRatio(strategyResults.ProfitList),
+		strategyResults.Profit)
+
+	assert.Equal(t, 1.2, ratio)
+	assert.Equal(t, -2.0870777152029802, profit)
+	assert.Equal(t, []float64{0.11500000000000003}, constants)
+}
