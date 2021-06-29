00793196f642b2a5ad6e86b7e4c929e6ed8a93b0
aoterocom <aoterolorenzo@gmail.com>
2021-06-29 23:00:30 +0200

    Update go.mod

--- strategies/stoch_rsi_custom_strategy.go
+++ strategies/stoch_rsi_custom_strategy.go
@@ -156,13 +156,13 @@ func (s *StochRSICustomStrategy) Analyze(pair string, exchangeService interfaces
 	helpers.Logger.Debugln(fmt.Sprintf("→ Analyzing %s", strings.Replace(reflect.TypeOf(s).String(), "*strategies.", "", 1)))
 
 	// Analyze last 1000 candles
-	result15m1000, err := s.PerformSimulation(pair, exchangeService, "15m", 2000, 0, nil)
+	result15m1000, err := s.PerformSimulation(pair, exchangeService, "1h", 2000, 0, nil)
 	if err != nil {
 		return nil, err
 	}
 	// Analyze last 500 candles
 	strategyAnalysis.StrategyResults = append(strategyAnalysis.StrategyResults, result15m1000)
-	result15m500, err := s.PerformSimulation(pair, exchangeService, "15m", 500, 0, &result15m1000.Constants)
+	result15m500, err := s.PerformSimulation(pair, exchangeService, "1h", 500, 0, &result15m1000.Constants)
 	if err != nil {
 		return nil, err
 	}
