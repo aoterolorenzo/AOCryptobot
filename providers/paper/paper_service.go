9899a2bef1c9a4d647bafa4704ce9a1aad8356b2
aoterocom <aoterolorenzo@gmail.com>
2021-08-18 14:45:05 +0200

    Update false benefit

--- providers/paper/paper_service.go
+++ providers/paper/paper_service.go
@@ -38,11 +38,11 @@ func init() {
 }
 
 func (paperService *PaperService) GetTotalBalance(asset string) (float64, error) {
-	return 10652.77, nil
+	return 10830.19, nil
 }
 
 func (paperService *PaperService) GetAvailableBalance(asset string) (float64, error) {
-	return 10652.77, nil
+	return 10830.19, nil
 }
 
 func (paperService *PaperService) GetLockedBalance(asset string) (float64, error) {
