239de78ad60951fd0826b5e7d24fb6a9ae37b16f
aoterocom <aoterolorenzo@gmail.com>
2021-09-08 13:17:02 +0200

    Fake balance

--- providers/paper/paper_service.go
+++ providers/paper/paper_service.go
@@ -38,11 +38,11 @@ func init() {
 }
 
 func (paperService *PaperService) GetTotalBalance(asset string) (float64, error) {
-	return 10539.64, nil
+	return 10670.73, nil
 }
 
 func (paperService *PaperService) GetAvailableBalance(asset string) (float64, error) {
-	return 10539.64, nil
+	return 10670.73, nil
 }
 
 func (paperService *PaperService) GetLockedBalance(asset string) (float64, error) {
