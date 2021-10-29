27b38923e97c73d9c852387b9a1408686d842363
aoterocom <aoterolorenzo@gmail.com>
2021-10-29 19:25:00 +0200

    Delegate strategy listing to main conf file

--- bot_signal-trader/signal-trader.go
+++ bot_signal-trader/signal-trader.go
@@ -2,6 +2,7 @@ package bot_signal_trader
 
 import (
 	"github.com/joho/godotenv"
+	log "github.com/sirupsen/logrus"
 	signalTrader "gitlab.com/aoterocom/AOCryptobot/bot_signal-trader/services"
 	"gitlab.com/aoterocom/AOCryptobot/helpers"
 	"gitlab.com/aoterocom/AOCryptobot/interfaces"
@@ -9,7 +10,6 @@ import (
 	binance2 "gitlab.com/aoterocom/AOCryptobot/providers/paper"
 	"gitlab.com/aoterocom/AOCryptobot/services"
 	strategies2 "gitlab.com/aoterocom/AOCryptobot/strategies"
-	"log"
 	"os"
 	"strings"
 )
@@ -18,6 +18,7 @@ type SignalTrader struct {
 }
 
 func init() {
+
 	cwd, _ := os.Getwd()
 	err := godotenv.Load(cwd + "/bot_signal-trader/conf.env")
 	if err != nil {
@@ -27,6 +28,12 @@ func init() {
 
 func (st *SignalTrader) Run() {
 	helpers.Logger.Infoln("🖖🏻 Signal Trader started")
+
+	strategiesString := os.Getenv("strategies")
+	strategiesList := strings.Split(strategiesString, ",")
+	if strategiesList[0] == "" {
+		helpers.Logger.Infoln("error: couldn't initialize bot. No strategies set")
+	}
 
 	targetCoin := os.Getenv("targetCoin")
 	whitelistCoinsString := os.Getenv("whitelistCoins")
@@ -44,23 +51,19 @@ func (st *SignalTrader) Run() {
 	var pairAnalysisResults []*analytics.PairAnalysis
 	bs := binance2.NewPaperService()
 	exchangeService := interfaces.ExchangeService(bs)
-	lun1MarCustomStrategy := strategies2.NewLun1MarCustomStrategy()
-	//lun5JulCustomStrategy := strategies2.NewLun5JulCustomStrategy()
-	//MACDCustomStrategy := strategies2.NewMACDCustomStrategy()
-	//stableStrategy := strategies2.NewStableStrategy()
-	stochRSICustomStrategy := strategies2.NewStochRSICustomStrategy()
-	//MACDInStochRSIOutCustomStrategy := strategies2.NewMACDInStochRSIOutCustomStrategy()
-	//stochRSIInMACDOutCustomStrategy := strategies2.NewStochRSIInMACDOutCustomStrategy()
 
-	strategies := []interfaces.Strategy{
-		&lun1MarCustomStrategy,
-		//&lun5JulCustomStrategy,
-		&stochRSICustomStrategy,
-		//&MACDCustomStrategy,
-		//&stableStrategy,
-		//&MACDInStochRSIOutCustomStrategy,
-		//&stochRSIInMACDOutCustomStrategy,
+	var strategies []interfaces.Strategy
+
+	for _, strategy := range strategiesList {
+		generatedStrategy, err := strategies2.StrategyFactory(strategy)
+		if err != nil {
+			helpers.Logger.Errorln(err)
+			log.Errorln(err)
+			os.Exit(1)
+		}
+		strategies = append(strategies, generatedStrategy)
 	}
+
 	marketAnalysisService := services.NewMarketAnalysisService(exchangeService, strategies, &pairAnalysisResults)
 	marketAnalysisService.PopulateWithPairs(targetCoin, whitelistCoins, blacklistCoins)
 	go marketAnalysisService.AnalyzeMarkets()
---
+++ strategies/strategy_factory.go
@@ -0,0 +1,36 @@
+package strategies
+
+import (
+	"fmt"
+	"gitlab.com/aoterocom/AOCryptobot/interfaces"
+)
+
+func StrategyFactory(strategyName string) (interfaces.Strategy, error) {
+
+	switch strategyName {
+	case "lun1MarCustomStrategy":
+		lun1MarCustomStrategy := NewLun1MarCustomStrategy()
+		return interfaces.Strategy(&lun1MarCustomStrategy), nil
+	case "lun5JulCustomStrategy":
+		lun5JulCustomStrategy := NewLun5JulCustomStrategy()
+		return interfaces.Strategy(&lun5JulCustomStrategy), nil
+	case "stableStrategy":
+		stableStrategy := NewStableStrategy()
+		return interfaces.Strategy(&stableStrategy), nil
+	case "MACDCustomStrategy":
+		MACDCustomStrategy := NewMACDCustomStrategy()
+		return interfaces.Strategy(&MACDCustomStrategy), nil
+	case "stochRSICustomStrategy":
+		stochRSICustomStrategy := NewStochRSICustomStrategy()
+		return interfaces.Strategy(&stochRSICustomStrategy), nil
+	case "MACDInStochRSIOutCustomStrategy":
+		MACDInStochRSIOutCustomStrategy := NewMACDInStochRSIOutCustomStrategy()
+		return interfaces.Strategy(&MACDInStochRSIOutCustomStrategy), nil
+	case "stochRSIInMACDOutCustomStrategy":
+		stochRSIInMACDOutCustomStrategy := NewStochRSIInMACDOutCustomStrategy()
+		return interfaces.Strategy(&stochRSIInMACDOutCustomStrategy), nil
+	default:
+		return nil, fmt.Errorf("%s is not a known strategy", strategyName)
+	}
+
+}
