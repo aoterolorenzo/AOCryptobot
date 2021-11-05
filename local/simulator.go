e48057d6a10b25004fc55b11f769a25d07f21054
aoterocom <aoterolorenzo@gmail.com>
2021-11-05 21:18:52 +0100

    Fix simulator.go

--- local/simulator.go
+++ local/simulator.go
@@ -1,16 +1,73 @@
 package main
 
 import (
+	"github.com/sdcoffey/big"
+	"github.com/sdcoffey/techan"
+	database "gitlab.com/aoterocom/AOCryptobot/database"
 	"gitlab.com/aoterocom/AOCryptobot/helpers"
 	"gitlab.com/aoterocom/AOCryptobot/interfaces"
+	"gitlab.com/aoterocom/AOCryptobot/models"
 	binance "gitlab.com/aoterocom/AOCryptobot/providers/paper"
 	"gitlab.com/aoterocom/AOCryptobot/strategies"
 	"log"
+	"os"
+	"time"
 )
 
 func main() {
 
-	targetCoin := "DOTEUR"
+	dbService := database.NewDBService("AOCryptoBot", "database-1.cxb3uopd6ntp.eu-west-3.rds.amazonaws.com", "3306", "admin", "abc123..")
+
+	start, _ := time.Parse("Jan 2, 2006 at 3:04pm (MST)", "Feb 4, 2014 at 6:05pm (PST)")
+	end, _ := time.Parse("Jan 2, 2006 at 3:04pm (MST)", "Feb 5, 2014 at 6:05pm (PST)")
+
+	candle := techan.Candle{
+		Period: techan.TimePeriod{
+			Start: start,
+			End:   end,
+		},
+		OpenPrice:  big.NewDecimal(0.1),
+		ClosePrice: big.NewDecimal(0.1),
+		MaxPrice:   big.NewDecimal(0.1),
+		MinPrice:   big.NewDecimal(0.1),
+		Volume:     big.NewDecimal(0.1),
+		TradeCount: 10000,
+	}
+
+	dbService.AddOrUpdateCandle(candle, "NEWEUR")
+
+	//
+	//candle = techan.Candle{
+	//	Period:     techan.TimePeriod{
+	//		Start: start,
+	//		End:   end,
+	//	},
+	//	OpenPrice:  big.NewDecimal(3.1),
+	//	ClosePrice: big.NewDecimal(3.1),
+	//	MaxPrice:   big.NewDecimal(3.1),
+	//	MinPrice:   big.NewDecimal(3.1),
+	//	Volume:     big.NewDecimal(3.1),
+	//	TradeCount: 10000,
+	//}
+	//
+	//dbService.AddOrUpdateCandle(candle, "NEW2EUR")
+
+	os.Exit(1)
+
+	dbOrder1 := models.NewEmptyOrder()
+	dbOrder1.StopPrice = "SISISI"
+	dbPosition := models.NewPosition(dbOrder1)
+
+	id := dbService.AddPosition(*dbPosition, "NEW", []float64{3.0, 2.1}, 0.0, 0.0, 0.0)
+
+	dbOrder2 := models.NewEmptyOrder()
+	dbOrder2.StopPrice = "TOMATOMATOMA"
+
+	dbPosition.Exit(dbOrder2)
+	dbService.UpdatePosition(id, *dbPosition, "NEW", []float64{3.0, 2.1}, 0.0, 0.0, 0.0)
+
+	os.Exit(1)
+	targetCoin := "EUR"
 	var whitelist []string
 	var blacklist []string
 	bs := binance.NewPaperService()
@@ -21,13 +78,13 @@ func main() {
 	//MACDCustomStrategy := strategies.NewMACDCustomStrategy()
 	//stableStrategy := strategies.NewStableStrategy()
 	//stochRSICustomStrategy := strategies.NewStochRSICustomStrategy()
-	stochRSIInMACDOutCustomStrategy := strategies.NewMACDInStochRSIOutCustomStrategy()
+	mixedStrategy1 := strategies.NewMixedStrategy1("1h")
 
 	selectedStrategies := []interfaces.Strategy{
 		//&MACDCustomStrategy,
 		//&lun1MarCustomStrategy,
 		//&stochRSICustomStrategy,
-		&stochRSIInMACDOutCustomStrategy,
+		&mixedStrategy1,
 		//&lun5JulCustomStrategy,
 		//&stableStrategy,
 	}
