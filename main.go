5931b2527543b7a11136e841962c9be48645a4e3
aoterocom <aoterolorenzo@gmail.com>
2022-09-22 19:14:44 +0200

    Add panic recovery

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"github.com/urfave/cli/v2"
 	botSignalTrader "gitlab.com/aoterocom/AOCryptobot/bot_signal-trader"
+	"gitlab.com/aoterocom/AOCryptobot/helpers"
 	"gitlab.com/aoterocom/AOCryptobot/interfaces"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
@@ -34,5 +37,13 @@ func main() {
 }
 
 func rubBot(b interfaces.MarketBot, c *cli.Context) {
+	// Panic recovery. Relaunch application.
+	defer func() {
+		if r := recover(); r != nil {
+			helpers.Logger.Errorln(fmt.Sprintf("Recovered. Error:\n %v", r))
+			time.Sleep(1 * time.Second)
+			rubBot(b, c)
+		}
+	}()
 	b.Run(c)
 }
