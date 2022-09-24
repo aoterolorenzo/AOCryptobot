4af8ad160b95861e636635515b728a10e0ff9659
Alberto Otero Lorenzo <aoterolorenzo@gmail.com>
2022-09-24 12:44:26 +0000

    Resolve "Error in strategy calcs"

--- strategies/lun1mar_custom_strategy.go
+++ strategies/lun1mar_custom_strategy.go
@@ -151,8 +151,6 @@ func (s *Lun1MarCustomStrategy) PerformSimulation(pair string, exchangeService i
 
 	//fmt.Printf("BEST CONSTANT COMBINATION FOUND: Entry Constant: %.8f Profit: %.4f%%\n",
 	//	selectedEntryConstant, highestBalance*100/2000-100)
-
-	strategyResults.Trend = series.Candles[len(series.Candles)-1].ClosePrice.Float() / series.Candles[0].ClosePrice.Float()
 	strategyResults.Profit = highestBalance*100/2000 - 100
 	strategyResults.ProfitList = bestProfitList
 	strategyResults.Period = limit - omit
--- strategies/stable_strategy.go
+++ strategies/stable_strategy.go
@@ -144,8 +144,6 @@ func (s *StableStrategy) PerformSimulation(pair string, exchangeService interfac
 	//fmt.Println(data2)
 	//fmt.Printf("BEST CONSTANT COMBINATION FOUND: Entry Constant: %.8f Profit: %.4f%%\n",
 	//	selectedEntryConstant, highestBalance*100/1000-100)
-
-	strategyResults.Trend = series.Candles[len(series.Candles)-1].ClosePrice.Float() / series.Candles[0].ClosePrice.Float()
 	strategyResults.Profit = highestBalance*100/1000 - 100
 	strategyResults.ProfitList = data2
 	strategyResults.Period = limit - omit
