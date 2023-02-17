aefcac5ac5e86336c24891a698e7e5826b2e0d33
aoterocom <aoterolorenzo@gmail.com>
2023-02-17 22:36:48 +0100

    Updates

--- services/multi_market_service.go
+++ services/multi_market_service.go
@@ -117,8 +117,13 @@ func (mms *MultiMarketService) SignalAnalyzer() {
 
 	for {
 		for _, pairAnalysis := range mms.PairAnalysisResults {
-			if pairAnalysis == nil || len(pairAnalysis.StrategiesAnalysis) == 0 {
-				helpers.Logger.Debugln("No PairAnalysisResults or no Strategies for " + pairAnalysis.Pair)
+			if pairAnalysis == nil {
+				helpers.Logger.Debugln("No Strategies for " + pairAnalysis.Pair)
+				continue
+			}
+
+			if len(pairAnalysis.StrategiesAnalysis) == 0 {
+				helpers.Logger.Debugln("No PairAnalysisResults for " + pairAnalysis.Pair)
 				continue
 			}
 
